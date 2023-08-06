896784af26b4c1071459bd756ebbe31f2e8a5519
Pavel <Pavel.peshkov36@mail.ru>
2023-08-06 11:46:34 +0000

    some changes

--- 11-add-client.sh
+++ 11-add-client.sh
@@ -47,7 +47,8 @@ echo ${CLIENT_PUBLIC_KEY} > ./"${userName}${PUB_KEY}"
 
 read SERVER_PUBLIC_KEY < /etc/wireguard/server.pub
 
-# We get the following client IP address
+# Получаем следующий IP адрес 
+
 read OCTET_IP < /etc/wireguard/last_used_ip.var
 if
 	[[ $OCTET_IP -ge 254 ]]; then
